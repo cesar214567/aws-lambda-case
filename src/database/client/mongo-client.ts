40f31f70b9789bda12096b8246b68eb440695f9f
cesar214567 <camgcamg11@gmail.com>
2022-10-25 13:34:40 -0500

    deleted env

--- src/database/client/mongo-client.ts
+++ src/database/client/mongo-client.ts
@@ -1,11 +1,12 @@
 // Import the MongoDB driver
 import {MongoClient} from 'mongodb';
+import * as dotenv from "dotenv";
+dotenv.config();
 
 
 export const connectToDatabase = async () => {
   // Define our connection string. Info on where to get this will be described below. In a real world application you'd want to get this string from a key vault like AWS Key Management, but for brevity, we'll hardcode it in our serverless function here.
-  const MONGODB_URI =
-  "mongodb+srv://cesar214567:masterking212@cluster0.zy8g2k3.mongodb.net/?retryWrites=true&w=majority"
+  const MONGODB_URI = process.env.MONGODB_URI!
   // Once we connect to the database once, we'll store that connection and reuse it so that we don't have to connect to the database on every request
   // Connect to our MongoDB database hosted on MongoDB Atlas
   const client = await MongoClient.connect(MONGODB_URI)
